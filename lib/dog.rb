db823a7201cffa8d2f4313e975aef95e518e77f1
Liz Burton <liz.burton147@gmail.com>
2017-11-29 20:00:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Dog
+
+  @@all = []
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    # binding.pry
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each { | dog | puts dog.name }
+  end
+
+end
